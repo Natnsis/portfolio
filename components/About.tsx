4a345ccc8d8dc34a5e5f51712b3b23f79a71f42f
Natnael <nsisay49@gmail.com>
2026-05-31 17:41:27 +0300

    feat: implement reusable Carousel component and integrate it into the About section

--- components/About.tsx
+++ components/About.tsx
@@ -3,13 +3,20 @@
 import { GlobeIcon } from "@phosphor-icons/react";
 import { Badge } from "./ui/badge";
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 
 const About = () => {
   return (
-    <div className="py-10 grid grid-cols-3">
+    <div className="py-10 grid grid-cols-2">
       <div>
-        <h1>About Me</h1>
-        <p>
+        <h1 className="text-4xl">About Me</h1>
+        <p className="text-lg pr-3">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut hic
           voluptate, delectus sed, laborum, quis neque iure debitis soluta
           fugiat reiciendis voluptatibus enim praesentium quidem aliquam
@@ -17,27 +24,56 @@ const About = () => {
         </p>
       </div>
 
-      <div className="flex flex-col gap-5 bg-red-200 px-10 py-5">
-        <Badge>
-          <GlobeIcon size={20} />
-        </Badge>
-        <h1>120%</h1>
+      <div className="flex flex-col gap-5 bg-primary-foreground px-10 py-5">
+        <div className="flex gap-5 items-center">
+          <div className="border p-3 rounded-full w-fit">
+            <GlobeIcon size={70} />
+          </div>
+          <h1 className="text-7xl">120%</h1>
+        </div>
+
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem odio
           harum fugit amet, vel saepe molestiae voluptas magni, eos, nostrum
           velit impedit animi? Magnam reiciendis ipsam dicta animi, ea pariatur.
         </p>
-        <Image alt="natnael" width={300} height={100} src="/project.webp" />
       </div>
 
-      <div className="bg-blue-200 px-5">
-        <div className="flex justify-center">
-          <Image alt="natnael" width={200} height={200} src="/another.webp" />
+      <div className="px-5 mt-20 col-span-2">
+        <div className="my-10">
+          <h1 className="text-4xl">Experience</h1>
+          <p className="text-lg pr-3">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut hic
+            voluptate, delectus sed, laborum, quis neque iure debitis soluta
+            fugiat reiciendis voluptatibus enim praesentium quidem aliquam
+            voluptatum eum natus ducimus!
+          </p>
         </div>
-        <div className="flex flex-col gap-5">
-          <h1>Experience</h1>
-          <div>hehe</div>
-        </div>
+        <div></div>
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem>
+              <div className="flex flex-col gap-5">
+                <h1 className="text-4xl">Experience</h1>
+                <div>hehe1</div>
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="flex flex-col gap-5">
+                <h1 className="text-4xl">Experience</h1>
+                <div>hehe</div>
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="flex flex-col gap-5">
+                <h1 className="text-4xl">Experience</h1>
+                <div>hehe</div>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
--- components/ProjectsCut.tsx
+++ components/ProjectsCut.tsx
@@ -9,7 +9,7 @@ const ProjectsCut = () => {
     <div className="py-20 px-20">
       <div className="grid grid-cols-3 grid-rows-3 gap-5">
         <div
-          className="p-5 col-span-2 row-span-1 h-40 bg-cover bg-center flex flex-col justify-between"
+          className="p-5 col-span-2 row-span-1 h-[40vh] bg-cover bg-center flex flex-col justify-between"
           style={{ backgroundImage: "url('/project.webp')" }}
         >
           <Badge className="text-lg rounded-full py-3 px-3">
@@ -23,27 +23,75 @@ const ProjectsCut = () => {
           </div>
         </div>
         <div
-          className="col-span-1 row-span-2  bg-cover bg-center"
+          className="col-span-1 row-span-2  bg-cover bg-center flex flex-col justify-between p-5"
           style={{ backgroundImage: "url('/project.webp')" }}
         >
-          hehe
+          <Badge className="text-lg rounded-full py-3 px-3">
+            Snippet And Boilerplate Manager
+          </Badge>
+          <div className="flex justify-end">
+            <Button variant="outline" size="sm">
+              <GlobeIcon size={20} />
+              Visit Site
+            </Button>
+          </div>
         </div>
         <div
-          className="bg-red-300 col-span-1 row-span-1 bg-cover bg-center"
+          className="bg-red-300 col-span-1 row-span-1 bg-cover bg-center flex flex-col justify-between p-5"
           style={{ backgroundImage: "url('/project.webp')" }}
-        ></div>
+        >
+          <Badge className="text-lg rounded-full py-3 px-3">
+            Snippet And Boilerplate Manager
+          </Badge>
+          <div className="flex justify-end">
+            <Button variant="outline" size="sm">
+              <GlobeIcon size={20} />
+              Visit Site
+            </Button>
+          </div>
+        </div>
         <div
-          className="bg-red-300 col-span-1 row-span-1 bg-cover bg-center"
+          className="bg-red-300 col-span-1 row-span-1 bg-cover bg-center flex flex-col justify-between p-5"
           style={{ backgroundImage: "url('/project.webp')" }}
-        ></div>
+        >
+          <Badge className="text-lg rounded-full py-3 px-3">
+            Snippet And Boilerplate Manager
+          </Badge>
+          <div className="flex justify-end">
+            <Button variant="outline" size="sm">
+              <GlobeIcon size={20} />
+              Visit Site
+            </Button>
+          </div>
+        </div>
         <div
-          className="bg-red-300 col-span-1 row-span-1 bg-cover bg-center"
+          className="bg-red-300 col-span-1 row-span-1 bg-cover bg-center flex flex-col justify-between p-5"
           style={{ backgroundImage: "url('/project.webp')" }}
-        ></div>
+        >
+          <Badge className="text-lg rounded-full py-3 px-3">
+            Snippet And Boilerplate Manager
+          </Badge>
+          <div className="flex justify-end">
+            <Button variant="outline" size="sm">
+              <GlobeIcon size={20} />
+              Visit Site
+            </Button>
+          </div>
+        </div>
         <div
-          className="bg-red-300 col-span-2 row-span-1 bg-cover bg-center"
+          className="bg-red-300 col-span-2 row-span-1 bg-cover bg-center flex flex-col justify-between p-5"
           style={{ backgroundImage: "url('/project.webp')" }}
-        ></div>
+        >
+          <Badge className="text-lg rounded-full py-3 px-3">
+            Snippet And Boilerplate Manager
+          </Badge>
+          <div className="flex justify-end">
+            <Button variant="outline" size="sm">
+              <GlobeIcon size={20} />
+              Visit Site
+            </Button>
+          </div>
+        </div>
       </div>
       <div className="flex justify-center mt-10 items-center text-xl">
         View All Projects <ArrowRightIcon size={32} />
