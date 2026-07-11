faf5da9f9ff869d45c676e0415852f8bee722650
Natnael <nsisay49@gmail.com>
2026-07-11 10:37:38 +0300

    status failed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
 const page = () => {
   return (
-    <div>
-      <div>hehe</div>
+    <div className="p-5">
+      <Header />
+      <Hero />
     </div>
   );
 };
