5250ec977f6c838e81450f9bf0d01da0e253d908
Natnael <nsisay49@gmail.com>
2026-07-11 09:49:29 +0300

    font and background setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import ProjectsCut from "@/components/ProjectsCut";
-
 const page = () => {
   return (
-    <>
-      <Header />
-      <Hero />
-      <ProjectsCut />
-      <About />
-      <Contact />
-      <Footer />
-    </>
+    <div>
+      <div>hehe</div>
+    </div>
   );
 };
 
